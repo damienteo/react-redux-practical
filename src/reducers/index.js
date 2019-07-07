83c4a2fdf954f392a115cb88d1e011e57e225b63
damienteo <ksteo89@gmail.com>
2019-07-07 14:47:30 +0800

    Created reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+  return [
+    { title: "Hello", duration: "4:05" },
+    { title: "Goodbye", duration: "2:15" },
+    { title: "See You", duration: "3:09" },
+    { title: "I'm Good", duration: "3:16" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
